3c540ad224b3a97ea149f46f91f28d6c12ae34eb
Jonno Katahanas <jonathan.katahanas@outlook.com>
2020-12-05 16:54:23 +1100

    Added app.js foundations

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('App starts')
+console.log('App starts');
+const todoList = [];
+const onTodoFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('form submit');
+    const $todo = $('input[name="todo"]');
+    console.log("val", $todo.val());
+    const todoItem = {title: $todo.val(), done: false};
+    todoList.push(todoItem);
+    console.log("todoList", todoList);
+
+};
+$(`#todoForm`).on("submit", onTodoFormSubmit);
